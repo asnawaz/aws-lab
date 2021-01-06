c1d5136405e0640a44c3c6be379bcf4c96cc5cef
asnawaz <46220070+asnawaz@users.noreply.github.com>
2021-01-06 17:11:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,105 @@
+provider "aws" {
+    shared_credentials_file = "$HOME/.aws/credentials"
+    region                  = "us-east-2"
+}
 
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+// first subnet
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+// second subnet
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+// adding internet gateway to the vpc
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_instance" "public-ec2" {
+    ami           = var.ami_id
+    instance_type = var.instance_type
+    subnet_id     = aws_subnet.public.id
+    key_name      = "notes-app-key-pair"
+    vpc_security_group_ids = [ aws_security_group.ec2-sg.id ]
+    associate_public_ip_address = true
+
+    tags = {
+        Name = "ec2-main"
+    }
+
+    depends_on = [ aws_vpc.main.id, aws_internet_gateway.gw.id ]
+
+    user_data = <<EOF
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install -y mysql-client 
+EOF
+}
+
+resource "aws_security_group" "ec2-sg" {
+  name        = "security-group"
+  description = "allow inbound access to the EC2 instance"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_parameter_group" "default" {
+  name   = "rds-pg"
+  family = "mysql8.0"
+
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [ aws_subnet.private.id ]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+
+resource "aws_security_group" "rds-sg" {
+  name        = "rds-security-group"
+  description = "allow inbound access to the database"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    // protocol    = "tcp"
+    // from_port   = 0
+    // to_port     = 3306
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+}
